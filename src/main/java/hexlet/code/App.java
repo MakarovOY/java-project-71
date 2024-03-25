f49d9e1354cb0ff9de2cace757e5253c970dde0e
Oleg Makarov <makarovolegbox@yandex.ru>
2024-03-25 23:34:14 +0300

    add comment in App

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,47 +5,47 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 
+@CommandLine.Command (name = "gendiff",version =  "gendiff 1.00", description = "Compares two configuration files and shows a difference.")
+public class App implements Callable {
 
-public class App {
-  //  @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
-   // boolean help;
+    private static boolean follow;
+    @CommandLine.Parameters( paramLabel ="filepath1", description = "path to first file")
+    private String filepath1;
+    @CommandLine.Parameters( paramLabel ="filepath2", description = "path to second file")
+    private String filepath2;
+
+    @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit." )
+    boolean help;
+    @CommandLine.Option(names = {"V", "--version"}, description = "Print version information and exit." )
+    boolean version;
+
+    @CommandLine.Option( names = {"-f", "--format"},  paramLabel = "format", description = "output format [default: stylish]")
+    File file;
+
     public static void main(String[] args) throws Exception  {
 
 
+App app = new App();
 
+       int something = new CommandLine(app ).execute("-h");
 
-      //  Scanner scanner = new Scanner(System.in);
-      //  if(scanner.next().equals("-h")){
-       // new CommandLine(new HelloCommand()).execute("-h");}
-//        Path writeFilePath1 = Paths.get("C:\\Users\\PC SAN\\IdeaProjects\\java-project-71\\app\\file1.json");
-//        try {
-//            Path createdFilePath = Files.createFile(writeFilePath1);
-//            System.out.println("Файл создан: " + createdFilePath);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        Path writeFilePath2 = Paths.get("C:\\Users\\PC SAN\\IdeaProjects\\java-project-71\\app\\file2.json");
-//        try {
-//            Path createdFilePath = Files.createFile(writeFilePath2);
-//            System.out.println("Файл создан: " + createdFilePath);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-        int something = new CommandLine(new Differ() ).execute("-h");
-      //  System.exit(something);
-
-        var str = Differ.generate("C:\\Users\\PC SAN\\IdeaProjects\\java-project-71\\app\\file1.json","C:\\Users\\PC SAN\\IdeaProjects\\java-project-71\\app\\file2.json");
+        app.call();
+      //  var str = Differ.generate("C:\\Users\\PC SAN\\IdeaProjects\\java-project-71\\app\\file1.json","C:\\Users\\PC SAN\\IdeaProjects\\java-project-71\\app\\file2.json");
 
 
     }
-
+    @Override
+    public Integer call() throws Exception{
+        System.out.println(Differ.generate("C:\\Users\\PC SAN\\IdeaProjects\\java-project-71\\app\\file1.json",
+                "C:\\Users\\PC SAN\\IdeaProjects\\java-project-71\\app\\file2.json"));
+        return 1;
+    }
 
 }
--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -8,29 +8,26 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 
-@CommandLine.Command (name = "gendiff",version =  "gendiff 1.00", description = "Compares two configuration files and shows a difference.")
+//@CommandLine.Command (name = "gendiff",version =  "gendiff 1.00", description = "Compares two configuration files and shows a difference.")
 public class Differ {
-
-    private static boolean follow;
-    @CommandLine.Parameters( paramLabel ="filepath1", description = "path to first file")
-  private String filepath1;
-  @CommandLine.Parameters( paramLabel ="filepath2", description = "path to second file")
-  private String filepath2;
-
-@CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit." )
-    boolean help;
-@CommandLine.Option(names = {"V", "--version"}, description = "Print version information and exit." )
-boolean version;
-
-@CommandLine.Option( names = {"-f", "--format"},  paramLabel = "format", description = "output format [default: stylish]")
-File file;
+//
+//    private static boolean follow;
+//    @CommandLine.Parameters( paramLabel ="filepath1", description = "path to first file")
+//  private String filepath1;
+//  @CommandLine.Parameters( paramLabel ="filepath2", description = "path to second file")
+//  private String filepath2;
+//
+//@CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit." )
+//    boolean help;
+//@CommandLine.Option(names = {"V", "--version"}, description = "Print version information and exit." )
+//boolean version;
+//
+//@CommandLine.Option( names = {"-f", "--format"},  paramLabel = "format", description = "output format [default: stylish]")
+//File file;
 
 //    @Override
 //    public  Integer call(){
@@ -42,45 +39,68 @@ File file;
     public static String generate (String filepath1, String filepath2 ) throws Exception{
         Path path1 = Paths.get(filepath1).toAbsolutePath().normalize();
         Path path2 = Paths.get(filepath2).toAbsolutePath().normalize();
-//        if (!Files.exists(path1)) {
-//            throw new Exception("File '" + path1 + "' does not exist");
-//        }
+
         String json1 = Files.readString(path1);
         String json2 = Files.readString(path2);
 
         ObjectMapper objectMapper1 = new ObjectMapper();
         ObjectMapper objectMapper2 = new ObjectMapper();
-//
-        InputFile inputFile1 = objectMapper1.readValue(json1, InputFile.class);
-        InputFile inputFile2 = objectMapper2.readValue(json2, InputFile.class);
 
 
-//    String hostDiff = " host: ";
-//       if(inputFile1.getHost() != null && inputFile2.getHost() != null){
-//           if(inputFile1.getHost().equals(inputFile2.getHost())){
-//            hostDiff = hostDiff + inputFile1.getHost();
-//              // System.out.println(hostDiff);
-//           }else{
-//
-//           }
-//       }
-        Map<String, Object> map1 = objectMapper1.readValue(json1, new TypeReference<Map<String,Object>>(){});
-        Map<String, Object> map2 = objectMapper1.readValue(json2, new TypeReference<Map<String,Object>>(){});
+        var map1 = objectMapper1.readValue(json1,Map.class);
+        var map2 = objectMapper2.readValue(json2,Map.class);
 
 
-        List<Map> listOfMaps = new ArrayList<>();
-        listOfMaps.add(map1);
-        listOfMaps.add(map2);
-       var byGroup =  listOfMaps.stream()
-                .collect(Collectors.groupingBy(map-> map.keySet()));
-        System.out.println(byGroup);
+        List<String> strings = new ArrayList<>();
+
+        map1.forEach((key,value)->{
+
+            strings.add(diff(map1,map2,key,value));
+        });
+        map2.forEach((key,value)->{
+            if(!map1.containsKey(key) && map2.containsKey(key)){
+
+            strings.add(diff(map1,map2,key,value));
+            }
+        });
+        StringBuilder stringBuilder =new StringBuilder();
+        Collections.sort(strings);
+
+        strings.stream()
+
+                .forEach(s ->{
+                    if (s.charAt(s.length()-1) == '+') {
+                        s= ("+" + s).substring(0,s.length());
+                    }
+                    if(s.charAt(s.length()-1) == '-') {
+                        s= ("-" + s).substring(0,s.length());
+                    }
+
+                    stringBuilder.append(s);});
 
 
+ return stringBuilder.toString() ;
+    }
+    public static <K,T> String diff(Map map1,Map map2, K key, T value) {
+        String differ = key + " :";
 
+        if (map1.containsKey(key) && map2.containsKey(key)) {
+            if (map1.get(key).equals(map2.get(key))) {
+                differ =  " " + differ + map1.get(key) + "\n_";
+            }else{
+                differ =  differ + map1.get(key) + "\n" + "+" +differ + map2.get(key)+ "\n_-"  ;
+            }
+        }
+        if(map1.containsKey(key) && !map2.containsKey(key)){
+            differ =  differ + map1.get(key)+ "\n_-";
 
+        }
+        if(!map1.containsKey(key) && map2.containsKey(key)){
+            differ =  differ + map2.get(key) + "\n_+";
 
+        }
 
- return "" ;
+        return differ.replaceAll("_","");
     }
 
 }
