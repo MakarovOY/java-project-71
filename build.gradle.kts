d037a0deee823fee2829a54b797cfc667cb57ec8
Oleg Makarov <makarovolegbox@yandex.ru>
2024-03-28 23:12:32 +0300

    add changes in Parser.java

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
 
     implementation ("info.picocli:picocli:4.7.5")
     implementation ("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation ( "com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml", "2.13.4")
     annotationProcessor ("info.picocli:picocli-codegen:4.7.5")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
---
+++ src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,43 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+//import com.fasterxml.jackson.dataformat.yaml.util.;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Parser {
+
+    public static Map parse (String filepath)throws Exception {
+        Path path = Paths.get(filepath).toAbsolutePath().normalize();
+
+        Map map = new HashMap<>();
+        String str = Files.readString(path);
+
+      String str1Arr =  filepath.substring(filepath.length()-4);
+      if(str1Arr.equals("json")){
+
+          ObjectMapper objectMapper1 = new ObjectMapper();
+
+
+
+           map = objectMapper1.readValue(str,Map.class);
+      }
+        if(str1Arr.equals("yaml")){
+            ObjectMapper mapper = new YAMLMapper();
+             map = mapper.readValue(str, Map.class);
+
+        }
+
+
+
+        return map;
+
+
+    }
+
+}
