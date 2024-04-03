fcace43d000199197a0a0ee2333a924b578800c6
Oleg Makarov <makarovolegbox@yandex.ru>
2024-04-03 23:02:02 +0300

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,7 @@ tasks.test {
 }
 tasks.jacocoTestReport {
     reports {
-        xml.required = false
-        csv.required = false
-        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+
+        xml.required.set(true)
     }
 }
