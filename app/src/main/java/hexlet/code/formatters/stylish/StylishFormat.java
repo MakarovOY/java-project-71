cd01fca66f56468f51d8f2b9ba047c93e94c5d73
Oleg Makarov <makarovolegbox@yandex.ru>
2024-04-24 23:10:41 +0300

    changes

--- app/src/main/java/hexlet/code/formatters/stylish/StylishFormat.java
+++ app/src/main/java/hexlet/code/formatters/stylish/StylishFormat.java
@@ -11,20 +11,20 @@ public class StylishFormat {
 
         resultMap.forEach((k, v) -> {
             if (v.equals(FileCondition.VALUE_OF_KEY_DOESNT_CHANGE)) {
-                stringBuilder.append("  " + k + ": " + map1.get(k) + "\n");
+                stringBuilder.append("   " + k + ": " + map1.get(k) + "\n");
             } else if (v.equals(FileCondition.VALUE_OF_KEY_WAS_CHANGED)) {
-                stringBuilder.append(" -" + k + ": " + map1.get(k) + "\n +" + k + ": " + map2.get(k) + "\n");
+                stringBuilder.append("  -" + k + ": " + map1.get(k) + "\n  +" + k + ": " + map2.get(k) + "\n");
 
             } else if (v.equals(FileCondition.KEY_WAS_DELETED)) {
-                stringBuilder.append(" -" + k + ": " + map1.get(k) + "\n");
+                stringBuilder.append("  -" + k + ": " + map1.get(k) + "\n");
 
             } else if (v.equals(FileCondition.KEY_WAS_ADDED)) {
-                stringBuilder.append(" +" + k + ": " + map2.get(k) + "\n");
+                stringBuilder.append("  +" + k + ": " + map2.get(k) + "\n");
             }
 
 
         });
-        return  "{   \n" + stringBuilder.toString() + "}";
+        return  "{\n" + stringBuilder.toString() + "}";
     }
 
 }
