668eea504e0c50595e86ba7b6e9c55c5828abee8
Oleg Makarov <makarovolegbox@yandex.ru>
2024-04-29 11:08:39 +0300

    Changes

--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParserTest {
     private Map expected = new HashMap();
-    @SuppressWarnings("MagicNumber")
+    @SuppressWarnings("magicnumber")
     @BeforeEach
             public void createMap() {
         expected.put("host", "hexlet.io");
